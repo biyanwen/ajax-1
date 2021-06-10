ff8a72f096b098cfe8e75d230d0e0e6d2578ea95
biyanwen <biyw@tsintergy.com>
2021-06-10 10:59:23 +0800

    更改请求路径

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 let getCss = document.querySelector('#getCSS');
 getCss.onclick = () => {
     const cssHttp = new XMLHttpRequest()
-    cssHttp.open('GET', "/style.css")
+    cssHttp.open('GET', "./style.css")
     cssHttp.onreadystatechange = () => {
         if (cssHttp.readyState === 4 && cssHttp.status === 200) {
             let styleElement = document.createElement('style');
@@ -15,7 +15,7 @@ getCss.onclick = () => {
 let getJS = document.querySelector('#getJS');
 getJS.onclick = () => {
     const jsHttp = new XMLHttpRequest()
-    jsHttp.open('GET', '/2.js')
+    jsHttp.open('GET', './2.js')
     jsHttp.onreadystatechange = () => {
         if (jsHttp.readyState === 4 && jsHttp.status === 200) {
             let scriptElement = document.createElement('script');
@@ -29,7 +29,7 @@ getJS.onclick = () => {
 let getHTML = document.querySelector('#getHTML');
 getHTML.onclick = () => {
     const htmlHttp = new XMLHttpRequest()
-    htmlHttp.open('GET', "/3.html")
+    htmlHttp.open('GET', "./3.html")
     htmlHttp.onreadystatechange = () => {
         if (htmlHttp.readyState === 4 && htmlHttp.status === 200) {
             let divElement = document.createElement('div');
@@ -43,7 +43,7 @@ getHTML.onclick = () => {
 let getXML = document.querySelector('#getXML');
 getXML.onclick = () => {
     const xmlHttp = new XMLHttpRequest()
-    xmlHttp.open('GET', "/4.xml")
+    xmlHttp.open('GET', "./4.xml")
     xmlHttp.onreadystatechange = () => {
         if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
             const dom = xmlHttp.responseXML;
@@ -58,7 +58,7 @@ getXML.onclick = () => {
 let getJson = document.querySelector('#getJSON')
 getJson.onclick = () => {
     const jsonHttp = new XMLHttpRequest()
-    jsonHttp.open('GET', "/5.json")
+    jsonHttp.open('GET', ".g/5.json")
     jsonHttp.onreadystatechange = () => {
         if (jsonHttp.readyState === 4 && jsonHttp.status === 200) {
             let parse = JSON.parse(jsonHttp.response);
